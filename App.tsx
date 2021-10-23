810815f0aae369ed959597443a3f4ef022a04232
Stefany Herrera <karen07stefanyh@gmail.com>
2021-10-22 21:07:07 -0600

    Cambio Moneda

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Cambio } from './src/components/Cambio';
+
+
+
+
+export default function App() {
+  return (
+    <Cambio />
+  
+  );
+}
+
+
+
